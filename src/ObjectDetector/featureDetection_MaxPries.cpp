96285b350a6c7c9f180fc96b59067ae3b7c78aea
Max Pries <118437972+maxwell6q@users.noreply.github.com>
2025-04-22 20:11:35 +0200

    Added featureDetection_MaxPries.cpp

---
+++ src/ObjectDetector/featureDetection_MaxPries.cpp
@@ -0,0 +1,42 @@
+#include "Utils/inputProcessing_MaxPries.h"
+#include "ObjectDetector/featureDetection_MaxPries.h"
+
+
+
+void computeFeaturesSingleImage(const cv::Mat &img, cv::Mat &descriptors, 
+    std::vector<cv::KeyPoint> &keypoints)
+{
+    // Create a cv::SIFT object, which is like a pointer
+    cv::Ptr<cv::SIFT> sift = cv::SIFT::create();
+    // cv::noArray() stands for an empty mask, 
+    sift->detectAndCompute(img, cv::noArray(), keypoints, descriptors);
+}
+
+
+void computeFullDescriptorList(const std::vector<std::string> pathList, 
+    const std::vector<std::string> maskList,
+    cv::Mat &descriptorList)
+{
+    // Create a cv::SIFT object, which is like a pointer
+    cv::Ptr<cv::SIFT> sift = cv::SIFT::create();
+
+    for (int i=0; i<pathList.size(); i++)
+    {
+        cv::Mat trainImg;
+        cv::Mat trainMask;
+        loadInput(trainImg, pathList[i]);
+        loadInput(trainMask, maskList[i], true);
+
+        std::vector<cv::KeyPoint> keypoints;
+        cv::Mat descriptors;
+
+        // Detect keypoints within the mask and computes their descriptors
+        sift->detectAndCompute(trainImg, trainMask, keypoints, descriptors);
+
+        // Concattenate the descriptors of each training image
+        if (!descriptors.empty())
+        {
+            descriptorList.push_back(descriptors);
+        }
+    }
+}
