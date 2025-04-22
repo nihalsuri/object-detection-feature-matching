fbd4f4c82480f92b4b74bde229b2b1bb25b5c886
Max Pries <118437972+maxwell6q@users.noreply.github.com>
2025-04-22 20:12:52 +0200

    Added featureMatching_MaxPries.cpp

---
+++ src/FeatureMatcher/featureMatching_MaxPries.cpp
@@ -0,0 +1,35 @@
+#include "FeatureMatcher/featureMatching_MaxPries.h"
+
+
+void matchFromDescriptors(const cv::Mat &dscrTest, const cv::Mat &dscrTrain, 
+    std::vector<std::vector<cv::DMatch>> &knnMatches, const int k)
+{
+    // Create BFMatcher object
+    cv::BFMatcher matcher;
+    // find the k best matches for each descriptor of the test image
+    matcher.knnMatch(dscrTest, dscrTrain, knnMatches, k);
+}
+
+
+void ratioTest(const std::vector<std::vector<cv::DMatch>> &matchesIn,
+    std::vector<cv::DMatch> &matchesOut)
+{
+    for (const std::vector<cv::DMatch>& match : matchesIn)
+    {
+        // If the best match is considerably better than the second best, keep it
+        // The threshold of 0.75 is given by D. Lowe in his paper (can change this)
+        if (match.size() == 2 && match[0].distance < 0.75 * match[1].distance)
+            matchesOut.push_back(match[0]);
+    }
+}
+
+
+void keypointSelection(std::vector<cv::KeyPoint> &goodKeypoints, 
+    std::vector<cv::DMatch> goodMatches, const std::vector<cv::KeyPoint> keypoints)
+{
+    for (cv::DMatch& match : goodMatches)
+    {
+        // select those keypoints, which have one clear best match
+        goodKeypoints.push_back(keypoints[match.queryIdx]);
+    }
+}
