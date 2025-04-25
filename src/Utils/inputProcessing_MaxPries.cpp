e30627b71c1e60a21136032e53d334a97c41d972
Max Pries <118437972+maxwell6q@users.noreply.github.com>
2025-04-25 18:50:40 +0200

    inputProcessing - Automation for all files

--- src/Utils/inputProcessing_MaxPries.cpp
+++ src/Utils/inputProcessing_MaxPries.cpp
@@ -19,7 +19,7 @@ void loadInput(cv::Mat &img, const std::string path, bool grayscale)
 }
 
 
-std::vector<std::string> fetchFilenames(const std::string sourceDir, const std::string ending)
+std::vector<std::string> fetchFilepaths(const std::string sourceDir, const std::string ending)
 {
     namespace fs = std::filesystem;
 
@@ -43,4 +43,34 @@ std::vector<std::string> fetchFilenames(const std::string sourceDir, const std::
         }
     }
     return pathList;
+}
+
+
+
+void loadTrainTestData(std::vector<std::vector<std::string>> &allModels,
+    std::vector<std::vector<std::string>> &allMasks,
+    std::vector<std::string> &allTests)
+{
+    // Collect all file-paths corresponding to each object and type of image
+    // sugar box
+    std::vector<std::string> pathListSugarModels = fetchFilepaths("../data/004_sugar_box/models", "color.png");
+    std::vector<std::string> pathListSugarMasks = fetchFilepaths("../data/004_sugar_box/models", "mask.png");
+    std::vector<std::string> pathListSugarTests = fetchFilepaths("../data/004_sugar_box/test_images", "color.jpg");
+
+    // mustard bottle
+    std::vector<std::string> pathListMustardModels = fetchFilepaths("../data/006_mustard_bottle/models", "color.png");
+    std::vector<std::string> pathListMustardMasks = fetchFilepaths("../data/006_mustard_bottle/models", "mask.png");
+    std::vector<std::string> pathListMustardTests = fetchFilepaths("../data/006_mustard_bottle/test_images", "color.jpg");
+
+    // power drill
+    std::vector<std::string> pathListDrillModels = fetchFilepaths("../data/035_power_drill/models", "color.png");
+    std::vector<std::string> pathListDrillMasks = fetchFilepaths("../data/035_power_drill/models", "mask.png");
+    std::vector<std::string> pathListDrillTests = fetchFilepaths("../data/035_power_drill/test_images", "color.jpg");
+
+    // Concattenate them based on type
+    allModels = {pathListSugarModels, pathListMustardModels, pathListDrillModels};
+    allMasks = {pathListSugarMasks, pathListMustardMasks, pathListDrillMasks};
+    allTests = pathListSugarTests;
+    allTests.insert(allTests.end(), pathListMustardTests.begin(), pathListMustardTests.end());
+    allTests.insert(allTests.end(), pathListDrillTests.begin(), pathListDrillTests.end());
 }
