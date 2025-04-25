da0fdf7c31dc49bdec6fa39d94835cb940797c1d
Max Pries <118437972+maxwell6q@users.noreply.github.com>
2025-04-25 18:50:14 +0200

    main - Automation for all files

--- src/main.cpp
+++ src/main.cpp
@@ -1,79 +1,67 @@
-#include <opencv2/highgui.hpp>
-#include <opencv2/imgproc.hpp>
-#include <opencv2/features2d.hpp>
-
 #include <iostream>
 #include <string>
 #include <vector>
-#include <filesystem>
 
 #include "Utils/inputProcessing_MaxPries.h"
-#include "Utils/OutputProcessing_BjornMagnus.h"
-#include "Utils/outputProcessing_NihalSuri.h"
-#include "ObjectDetector/featureDetection_MaxPries.h"
-#include "FeatureMatcher/featureMatching_MaxPries.h"
-#include "ObjectDetector/ObjectDetector_BjornMagnus.h"
+#include "ObjectDetector/objectDetectorAll_MaxPries.h"
 
 
  
+
+
+
 int main(int argc, char** argv)
 {
-    //Collect all filenames from a given directory
-    std::vector<std::string> pathListSugarModels = fetchFilenames("../data/004_sugar_box/models", "color.png");
-    std::vector<std::string> pathListSugarMask = fetchFilenames("../data/004_sugar_box/models", "mask.png");
-    std::vector<std::string> pathListSugarTest = fetchFilenames("../data/004_sugar_box/test_images", "color.jpg");
 
-
-    //Compute the list of all descriptors for the model "sugar_box"
-    cv::Mat descriptorList;
-    computeFullDescriptorList(pathListSugarModels, pathListSugarModels, descriptorList);
-
+    std::vector<std::vector<std::string>> allModels, allMasks;
+    std::vector<std::string> allTests;
+    loadTrainTestData(allModels, allMasks, allTests);
     
 
-    //Load one test image and compute its keypoints
-    cv::Rect objectDeteced,objectDeteced1,objectDeteced2;
-    std::string imagePath = pathListSugarTest[8];
-    std::vector<cv::KeyPoint> goodKeypoints;
+    int mode = 0;
+    int minKeypoints = 10;
     double sigma = 1;
-    double cutOffRatio = 0.77;
+    double cutOffRatio = 0.78;
+    bool logImages = true;
+    runDetectionAllImages(allModels, allMasks, allTests, mode, minKeypoints, sigma, cutOffRatio, logImages);
 
-    //Find position of rectangle that fit most keypoints
-    runDetection(imagePath, descriptorList, 0, 12 , objectDeteced, goodKeypoints, sigma, cutOffRatio);
-    //Find position of rectangl that fit most keypoints and centre the rectangle around the keypoints 
-    runDetection(imagePath, descriptorList, 1, 12 , objectDeteced1, goodKeypoints, sigma, cutOffRatio);
-    //Find all the object based on canny edge detection and find the object containing the most keypoints
-    runDetection(imagePath, descriptorList, 2, 12 , objectDeteced2, goodKeypoints, sigma, cutOffRatio);
-    
-    //Draw the good keypoints
-    cv::Mat testImage;
-    loadInput(testImage, imagePath);
-    cv::Mat outImage;
-    cv::Scalar color( 0, 0, 255 );
-    cv::drawKeypoints(testImage, goodKeypoints, outImage, color);
+    /*
+    int bestMode = 0;
+    int bestMinKeypoints = 9;
+    double bestSigma = 1;
+    double bestCutOffRatio = 0.73;
+    double bestScore = 0;
 
-    //Draw the Boxes 
-    cv::rectangle(outImage, objectDeteced, cv::Scalar(0, 0, 255),2);
-    cv::rectangle(outImage, objectDeteced1, cv::Scalar(0, 255, 0),2);
-    cv::rectangle(outImage,objectDeteced2, cv::Scalar(255, 0, 0),2);
+    //NOTE: THIS WILL RUN THE WHOLE THING 216 TIMES!!
+    for (int mode=bestMode; mode<=2; mode++)
+    {
+        for (int minKeypoints=bestMinKeypoints; minKeypoints<=15; minKeypoints+=2)
+        {
+            for (double sigma=vestSigma; sigma<=2; sigma+=0.5)
+            {
+                for (double cutOffRatio=bestCutOffRatio; cutOffRatio<=0.78; cutOffRatio+=0.01)
+                {
+                    runDetectionAllImages(allModels, allMasks, allTests, mode, minKeypoints, sigma, cutOffRatio, false);
+                    currentScore = computeScoreMetric
 
-    //Open image
-    openImage(outImage,1,"Test");
-
-    //Save image with original name + _output.jpg saved to given path 
-    saveImage(outImage,imagePath,"../results/images/","_output.jpg");
-
-    // write to files for each bounding box method, choose one of them in the end
-    std::vector<cv::Rect>mode0;
-    mode0.push_back(objectDeteced); 
-    writeCoordinates(mode0, imagePath, "../results/coordinates/mode0/", "_output"); 
-
-    std::vector<cv::Rect>mode1;
-    mode1.push_back(objectDeteced1); 
-    writeCoordinates(mode1, imagePath, "../results/coordinates/mode1/", "_output");
-
-    std::vector<cv::Rect>mode2;
-    mode2.push_back(objectDeteced2); 
-    writeCoordinates(mode2, imagePath, "../results/coordinates/mode2/", "_output");
+                    if (currentScore > bestScore)
+                    {
+                        bestScore = currentScore;
+                        bestMode = mode;
+                        bestMinKeypoints = minKeypoints;
+                        bestSigma = sigma;
+                        bestCutOffRatio = cutOffRatio;
+                    }
+                }
+                std::cout << "Current Best Score:     " << bestScore <<std::endl;
+                std::cout << "Current Best Mode:      " << bestMode <<std::endl;
+                std::cout << "Current Best Keypoints: " << bestMode <<std::endl;
+                std::cout << "Current Best Sigma:     " << bestMode <<std::endl;
+                std::cout << "Current Best Ratio:     " << bestMode <<std::endl;
+            }
+        }
+    }
+    */
 
     return 0;
 }
