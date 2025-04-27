9ee19897f8cbcaea13ca87df6fce02a33cf2470b
Max Pries <118437972+maxwell6q@users.noreply.github.com>
2025-04-27 18:55:59 +0200

    cleaned main to hand it in

--- src/main.cpp
+++ src/main.cpp
@@ -9,47 +9,35 @@
 
 int main(int argc, char** argv)
 {
-    
+    // Best parameters SIFT
+    int detector = 0;                       // 0:SIFT, 1:SURF, 2:ORB, 3:AKAZE
+    int mode = 2;                           // The type of box-fitting
+    int minKeypoints = 10;                  // Min number of Keypoints to draw the box
+    double sigma = 0.8;                     // For gaussian smoothing of test images
+    double cutOffRatio = 0.83;              // For ratio test
+    int boxHeight = 225;                    // Box dimension 1 (might rotate)
+    int boxWidth = 150;                     // Box dimension 2,(might rotate)
+    bool logImages = true;                  // Save images with KPs and boxes (y/n)
+    std::string savePath = "../results";    // Where to save results (will always log txt data)
+
+
+    // Declare the targets for training and test data
     std::vector<std::vector<std::string>> allModels, allMasks;
     std::vector<std::string> allTests;
+
+
+    // Load training and test data from fixed path
     loadTrainTestData(allModels, allMasks, allTests);
 
-    
-    // Best parameters SIFT
-    int mode = 2;
-    int minKeypoints = 10;       // up to 10 to balance score and flase positives
-    double sigma = 0.8;
-    double cutOffRatio = 0.80;   // 0.80 to balance score and flase positives (best score at 0.83)
-    int boxHeight = 225;
-    int boxWidth = 150;
 
-    bool logImages = true;
-    std::string savePath = "../results";
-    runDetectionAllImages(allModels, allMasks, allTests, mode, minKeypoints, sigma, cutOffRatio, boxHeight, boxWidth, logImages, savePath, 0);
+    // Process all images and store the results in the defined path
+    runDetectionAllImages(allModels, allMasks, allTests, mode, minKeypoints, sigma, cutOffRatio,
+                          boxHeight, boxWidth, logImages, savePath, detector);
+
+    
+    // Evaluate the mIoU and detection accuracy and display them on terminal
     performanceMetrics(savePath);
     
-    
-    /*
-    // Fixed size centerd box, fixed keypoints to 10. Only vary sigma and ratio
-    int mode = 2;
-    int boxWidth = 150;
-    int boxHeight = 225;
-    std::string savePath = "../results";
-    int startKeypoints = 10, stopKeypoints = 10;
-
-    double startSigma = 0.5, stopSigma = 1.5;
-    double startRatio = 0.70, stopRatio = 0.85;
-
-    std::cout << "SURF:";
-    evaluateBestParameters(mode, savePath, startKeypoints, stopKeypoints, startSigma, stopSigma, 
-                           startRatio, stopRatio, boxHeight, boxHeight, boxWidth, 1);
-    std::cout << "\n\nORB:";
-    evaluateBestParameters(mode, savePath, startKeypoints, stopKeypoints, startSigma, stopSigma, 
-                           startRatio, stopRatio, boxHeight, boxHeight, boxWidth, 2);
-    std::cout << "\n\nAKAZE:";
-    evaluateBestParameters(mode, savePath, startKeypoints, stopKeypoints, startSigma, stopSigma, 
-                           startRatio, stopRatio, boxHeight, boxHeight, boxWidth, 3);
-    */
 
     return 0;
 }
